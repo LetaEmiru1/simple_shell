4fb52057b739cc167e0e00f276048b3749942078
Leta Malimo <letaemiruleta@gmail.com>
2023-12-13 22:23:52 +0300

    Header file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#define MAX_INPUT 1024
+#define MAX_ARG_SIZE 128
+#define MAX_ARGS 32
+int path_finder(int ac, char **av);
+
+#endif
